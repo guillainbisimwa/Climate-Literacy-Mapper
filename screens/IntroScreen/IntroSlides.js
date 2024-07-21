fec5b40394791f979274d189e9ae22a7b5312bac
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-07-21 15:30:00 +0200

    Add Onboard components

---
+++ screens/IntroScreen/IntroSlides.js
@@ -0,0 +1,26 @@
+//slide
+const slides = [
+  {
+    id: 1,
+    lable: 'WELCOME',
+    subtitle: 'TEXT TEXT TEX',
+    des: 'Text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text',
+    imageUrl: require('./../../assets/images/il5.png'),
+  },
+  {
+    id: 2,
+    lable: 'CLIMATE DATA',
+    subtitle: 'Text text text1',
+    des: 'Text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text',
+    imageUrl: require('./../../assets/images/il1.png'),
+  },
+
+  {
+    id: 3,
+    lable: 'MAPPER',
+    subtitle: 'Text text text2',
+    des: 'Text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text',
+    imageUrl: require('./../../assets/images/il2.png'),
+  },
+];
+export default slides;
