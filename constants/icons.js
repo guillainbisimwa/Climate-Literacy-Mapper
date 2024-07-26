d21b9b23f7d0b68229b6985edacf075955a8bc6d
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-07-26 17:13:28 +0200

    Add icons

--- constants/icons.js
+++ constants/icons.js
@@ -1,6 +1,6 @@
-export const back_arrow = require('../assets/icons/back_arrow_icon.png');
-export const pin = require('../assets/icons/pin.png');
-export const search = require('../assets/icons/search.png');
+export const back_arrow = require('../assets/icons/files/back_arrow_icon.png');
+export const pin = require('../assets/icons/files/pin.png');
+export const search = require('../assets/icons/files/search.png');
 
 export default {
   back_arrow,
