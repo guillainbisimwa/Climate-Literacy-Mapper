b56b254e69797fe5c3ae48e43cc8b5998365138a
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-07-28 22:21:14 +0200

    Add emply profile screen

---
+++ screens/Main/ProfileScreen.js
@@ -0,0 +1,9 @@
+import {Block, Text} from  "../../components"
+
+const ProfileScreen = () => {
+    return <Block>
+        <Text>ProfileScreen SCREEN</Text>
+    </Block>
+}
+
+export default ProfileScreen
